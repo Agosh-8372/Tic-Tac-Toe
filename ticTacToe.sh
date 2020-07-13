c00e6ea0f2bfddf315c19a219df13f7054b63584
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-13 18:44:49 +0530

    [agosh]ADD:resetting the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
+NUM_ROWS=3
+NUM=COLS=3
+BOARD_SIZE=$((NUM_ROWS*NUM_COLS))
 
+position=0
+declare -a playBoard
+function resetPlayBoard()
+
+{
+	for((position=1;position<=BOARD_SIZE;position++))
+	do
+		playBoard[$position]=0;
+	done
+}
+resetPlayBoard
