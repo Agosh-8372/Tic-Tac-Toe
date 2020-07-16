c1c84b37fdb149ff2fd61b48320c239af8c49b2b
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-16 09:41:21 +0530

    [agosh]ADD:Check for win position,then play that move

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -4,6 +4,7 @@ NUM=COLS=3
 userLetterTemp=0
 chance=""
 player=0
+flag=0
 computer=0
 gameStop=0
 countX=0
@@ -19,83 +20,56 @@ function resetPlayBoard()
 
 function toss()
 {
-random=$((RANDOM%2))
-if [[ $random -eq 0 ]]
-then
-	chance="player";
-	((countX++))
-	read -p "Players chance first,choose your letter X or O " input
-	if [[ $input == X ]]
+	random=$((RANDOM%2))
+	if [[ $random -eq 0 ]]
 	then
-		userLetter=X;
+		chance="player";
+		((countX++))
+		read -p "Players chance first,choose your letter X or O " input
+		if [[ $input == X ]]
+		then
+			userLetter=X;
+		else
+			userLetter=O;
+		fi
 	else
-		userLetter=O;
+		echo "Computers chance first"
+		chance="computer";
+		((countO++))
+		choose=$((RANDOM%2))
+		if [ $choose -eq 0 ]
+		then
+			compLetter=O;
+		else
+			compLetter=X;
+		fi
 	fi
-else
-	echo "Computers chance first"
-	chance="computer";
-	((countO++))
-	choose=$((RANDOM%2))
-	if [ $choose -eq 0 ]
-	then
-		compLetter=O;
-	else
-		compLetter=X;
-	fi
-fi
 }
 
 function symbolAssign()
 {
-if [[ $chance == player &&  $userLetter == X ]]
-then
-	#userLetterTemp=X
-	compLetter=O
-#elif [[ $chance == player &&  $userLetterTemp == O ]]
-#then
-else
-	#userLetter=O
-	compLetter=X
-fi
-if [[ $chance == computer &&  $compLetter == X ]]
-then
-	#compLetter=X
-	userLetter=O
-else
-	#compLetter=O
-	userLetter=X
-fi
-}
-function computerPlay(){
-	val=$(((RANDOM%9)+1))
- computerMove $val
-}
-function checkDraw()
-{
-   if [[ $countX -eq 5 && $countO -eq 4 ]] || [[ $countX -eq 4 && $countO -eq 5 ]]
-   then
-      echo "its a draw"
-      gameStop=1;
-   elif [[ $countX -eq 4 && $countO -eq 5 ]]
-   then
-      echo "its a draw"
-      gameStop=1;
-   fi
+	if [[ $chance == player &&  $userLetter == X ]]
+	then
+		compLetter=O
+	else
+		compLetter=X
+	fi
+	if [[ $chance == computer &&  $compLetter == X ]]
+	then
+		userLetter=O
+	else
+		userLetter=X
+	fi
 }
 
-function chanceSwitch()
+function displayBoard()
 {
-if [[ $chance == player ]]
-then
-	((countO++))
-	computer
-elif [[ $chance == computer ]]
-then
-	((countX++))
-	user
-else
-	echo "Error"
-fi
+echo "Game Board At Display"
+for (( count=0;count<${#playBoard[@]};))
+do
+   echo "|${playBoard[count]}|${playBoard[count+1]}|${playBoard[count+2]}|"
+   count=$((count+3))
+done
 }
 
 function user()
@@ -110,14 +84,19 @@ chance="computer"
 userLetterTemp=$compLetter
 }
 
-function displayBoard()
+function chanceSwitch()
 {
-echo "Game Board At Display"
-for (( count=0;count<${#playBoard[@]};))
-do
-	echo "|${playBoard[count]}|${playBoard[count+1]}|${playBoard[count+2]}|"
-	count=$((count+3))
-done
+if [[ $chance == player ]]
+then
+   ((countO++))
+   computer
+elif [[ $chance == computer ]]
+then
+   ((countX++))
+   user
+else
+   echo "Error"
+fi
 }
 
 function checkChance()
@@ -130,6 +109,19 @@ do
 		break;
 	fi
 done
+}
+
+function checkDraw()
+{
+   if [[ $countX -eq 5 && $countO -eq 4 ]] || [[ $countX -eq 4 && $countO -eq 5 ]]
+   then
+      echo "its a draw"
+      gameStop=1;
+   elif [[ $countX -eq 4 && $countO -eq 5 ]]
+   then
+      echo "its a draw"
+      gameStop=1;
+   fi
 }
 
 function checkWin()
@@ -152,33 +144,72 @@ checkWin 2 5 8
 checkWin 0 4 8
 checkWin 2 4 6
 }
-function userInputPosition()
+
+function computerMove()
 {
-echo "players chance,Please enter a position to insert"
-read user_input
-yourMove $user_input
+c=$1
+if [[ ${playBoard[$((c))]} == "-" ]]
+then
+   playBoard[$((c))]=$compLetter
+else
+   compInputValue
+fi
+}
+
+function compInputValue(){
+ cval=$((RANDOM%9))
+computerMove $cval
+}
+
+
+function checkWinPos(){
+   if [[ ${playBoard[$1]} == "-" && ${playBoard[$2]} == $compLetter && ${playBoard[$3]} == $compLetter ]]
+   then
+      playBoard[$1]=$compLetter
+      flag=1
+   elif [[ ${playBoard[$2]} == "-" && ${playBoard[$1]} == $compLetter && ${playBoard[$3]} == $compLetter ]]
+   then
+      playBoard[$2]=$compLetter
+      flag=1
+   elif [[ ${playBoard[$3]} == "-" && ${playBoard[$1]} == $compLetter && ${playBoard[$2]} == $compLetter ]]
+   then
+      playBoard[$3]=$compLetter
+      flag=1
+   fi
+}
+
+
+function computerPlay(){
+   checkWinPos 0 1 2
+   checkWinPos 3 4 5
+   checkWinPos 6 7 8
+   checkWinPos 0 3 6
+   checkWinPos 1 4 7
+   checkWinPos 2 5 8
+   checkWinPos 0 4 8
+   checkWinPos 2 4 6
+   if [ $flag -eq 0 ]
+   then
+   compInputValue
+   fi
 }
 
 function yourMove()
 {
-pos=$1
-if [[ ${playBoard[$((pos-1))]} == "-" ]]
-then
-	playBoard[$((pos-1))]=$userLetter
-else
-	userInputPosition
-fi
+	pos=$1
+	if [[ ${playBoard[$((pos-1))]} == "-" ]]
+	then
+   	playBoard[$((pos-1))]=$userLetter
+	else
+   	userInputPosition
+	fi
 }
 
-function computerMove()
+function userInputPosition()
 {
-pos=$1
-if [[ ${playBoard[$((pos-1))]} == "-" ]]
-then
-   playBoard[$((pos-1))]=$compLetter
-else
-   computerPlay
-fi
+	echo "players chance Please enter a position to insert"
+	read user_input
+	yourMove $user_input
 }
 
 function gamePlay()
